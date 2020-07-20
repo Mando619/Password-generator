8744333b4c8a328895178df2ef392b6d971ee916
Armando Estrada <libryum33@hotmail.com>
2020-07-20 13:01:24 -0700

    added new loops and arrays

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); 
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "0123456789";
-var special = '!@#$%^&*()_+';
+
+// my arrays used for generating password
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const number = '0123456789'.split('');
+const special = '!@#$%^&*()_+';
 
 
-var userAnswers = {};
+    
+      const useLowerCase = confirm('Do you want to use lower case letters?');
+      const useUpperCase = confirm('Do you want to use uppercase letters?');
+      const useNumber = confirm('Do you want to use numbers? ');
+      const useSpecial = confirm('Do you want to use characters?');
 
-function promptUser() {
-  var lowerChar = confirm('Would you like to use lowercase characters?');
-  var upperChar = confirm('Would you like to use uppercase characters?');
-  var specialChar = confirm('Would you like to use special characters?');
-  var numberChar = confirm('Would you like to use number characters?');
+      let password = [];
 
+          if (useLowerCase) {
+            for (let i = 0; i < 2; i++) {
+                password.push('randomCharacter')
+            }
+          }
 
-  if (lowerChar) {
-    userAnswers[lowerCase] = lowercase;
+          if (useUpperCase) {
+            password = password.concat(upperCase);
+          }
+
+          if (useNumber) {
+            password = password.concat(useNumber);
+          }
+
+          if (useSpecial) {
+            password = password.concat(useSpecial);
+          }
+            console.log(shuffle(password).join(''));
+
+            function shuffle(array) {
+              var currentIndex = array.length, temporaryValue, randomIndex;
+
+                // shufflees elements that remain
+              while (0 !== currentIndex) {
+
+               // To pick a remaining element 
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                //to switch it with a current element
+                TemporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+              }
+
+            return array;
+
+            }
+
+// setting up password length
+  let passLength = parseInt(prompt("How long do you want your password to be? Please keep your password between 8 and 128 characters"));
+  while(passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    passLength = prompts("Im sorry, please input an integer between 8 and 138.")
   }
+    console.log('Password length is ' + passLength);
 
-  if (upperChar) {
-    userAnswers[upperCase] = upperCase;
-  }
 
-  if (specialChar) {
-    userAnswers[special] = special;
 
-  }
 
-  if (numberChar) {
-    userAnswers[number] = number;
-  }
 
-  console.log(userAnswers);
-  }
 
-  promptUser();
+
 
 // Write password to the #password input
 function writePassword() {
